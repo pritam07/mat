1aaea1e645737aa4a6087e0a52eca0162637eab3
pritam07 <bapittu@gmail.com>
2017-12-17 23:03:14 +0530

    new commit

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./common.component.css']
 })
 export class CommonComponent implements OnInit {
-
+  isVmenuOpen: boolean = true;
   constructor() { }
 
   ngOnInit() {
